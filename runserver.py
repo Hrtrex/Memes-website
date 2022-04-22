7ec311ca5ec2c952c4d6c1d491f7f46b44957cbd
Hrtrex <59179374+Hrtrex@users.noreply.github.com>
2022-04-22 22:24:21 +0200

    init_2

--- runserver.py
+++ runserver.py
@@ -5,10 +5,5 @@ import os
 #from App.memes import get_urls
 
 if __name__ == '__main__':
-    HOST = os.environ.get('SERVER_HOST', 'localhost')
-    try:
-        PORT = int(os.environ.get('SERVER_PORT', '5555'))
-    except ValueError:
-        PORT = 5555
-    app.run(HOST, PORT, debug=True)
+    app.run()
     
