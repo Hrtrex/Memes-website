2eb292c656edd881b44506f5bab8fe828f2b2307
Hrtrex <59179374+Hrtrex@users.noreply.github.com>
2022-05-23 01:57:16 +0200

    debug memes

--- App/memes.py
+++ App/memes.py
@@ -10,7 +10,7 @@ usr_agent = {
     'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11'
 }
 
-def get_urls_jbzd(page):
+def get_urls_jbzd(page=''):
 
     # url query string
     searchurl = JBZD + page
--- App/views.py
+++ App/views.py
@@ -6,16 +6,22 @@ from App.memes import get_urls_jbzd, get_urls_kwejk
 def home():
     return "<h1>Home page</h1>"
 
+@app.route("/<name>")
+def user(name):
+    return render_template("index.html", name=name)
+
 @app.route("/admin")
 def admin():
     return redirect(url_for("home"))
 
+@app.route("/jbzd/", defaults={'page': ''})
 @app.route("/jbzd/<page>")
 def jbzd(page):
     urls, votes = get_urls_jbzd(page)
     data = list(zip(urls, votes))
     return render_template("index.html", test=data)
 
+@app.route("/kwejk/", defaults={'page': ''})
 @app.route("/kwejk/<page>")
 def kwejk(page):
     urls, votes = get_urls_kwejk(page)
