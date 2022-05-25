8a9f202ee9cd150436c4f22bc3672d32b16973aa
Hrtrex <59179374+Hrtrex@users.noreply.github.com>
2022-05-25 21:51:16 +0200

    Rankings (#3)
    * dodane polaczenie z abza i funkcja sortujaca po ocenach memow
    
    * Add files via upload
    
    front
    
    * Create logo.png
    
    * Add files via upload
    
    * Add files via upload
    
    * Add files via upload
    
    * Add files via upload
    
    * nth
    
    * nth2
    
    * added memes
    
    Co-authored-by: ArekHanejko <arekimmobile@gmail.com>
    Co-authored-by: ArekHanejko <81444898+ArekHanejko@users.noreply.github.com>

--- App/views.py
+++ App/views.py
@@ -1,22 +1,48 @@
 from flask import Flask, redirect, url_for, render_template
 from App import app
-from App.db import get_db_connection
+import psycopg2
+from App.memes import get_urls_jbzd, get_urls_kwejk
+
+def get_db_connection():
+    conn = psycopg2.connect(host='flask-server.postgres.database.azure.com',
+                            database='db',
+                            user='hrtrex',
+                            password='Jebacdisa_12',
+                            sslmode='require')
+    return conn
 
 @app.route("/")
 def home():
-    return "<h1>Home page</h1>"
+    return render_template("index.html")
+
+@app.route("/memesRanking")
+def getMemesSortedByRatings():
+    conn = get_db_connection()
+    cur = conn.cursor()
+    cur.execute('select id_mema,tytul,kategoria,CAST(avg(jaka_ocena) as NUMERIC(10,1)) srednia from oceny_memow,memy where memy.id_mema=oceny_memow.Memy_id_mema group by id_mema having avg(jaka_ocena) is not null order by srednia desc;')
+    memy = cur.fetchall()
+    cur.close()
+    conn.close()
+    return render_template('memesRanking.html', memy=memy)
 
 @app.route("/<name>")
 def user(name):
-    conn = get_db_connection()
-    temps = conn.execute('SELECT * from blokady').fetchall()
-    conn.close()
-    return render_template("index.html", name=name, sqls=temps)
+    return render_template("index.html")
 
 @app.route("/admin")
 def admin():
     return redirect(url_for("home"))
 
+@app.route("/jbzd/", defaults={'page': ''})
 @app.route("/jbzd/<page>")
-def jbzd():
-    return render_template("index.html")
+def jbzd(page):
+    urls, votes = get_urls_jbzd(page)
+    data = list(zip(urls, votes))
+    return render_template("memy.html", links=data)
+
+@app.route("/kwejk/", defaults={'page': ''})
+@app.route("/kwejk/<page>")
+def kwejk(page):
+    urls, votes = get_urls_kwejk(page)
+    data = list(zip(urls, votes))
+    return render_template("memy.html", links=data)
