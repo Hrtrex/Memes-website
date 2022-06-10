b931abf9a70e3b98095a6508d4337c8febbb564f
Bl0tniaQus <34137984+Bl0tniaQus@users.noreply.github.com>
2022-06-10 16:37:38 +0200

    chcieliscie nielokalną baze to macie

--- App/views.py
+++ App/views.py
@@ -4,11 +4,11 @@ import psycopg2
 from App.memes import get_urls_jbzd, get_urls_kwejk
 
 def get_db_connection():
-    conn = psycopg2.connect(host='localhost',
-                            database='memy',
-                            user='postgres',
-                            password='D-upa123'
-                            )
+    conn = psycopg2.connect(host='flask-server.postgres.database.azure.com',
+                            database='db',
+                            user='hrtrex',
+                            password='Jebacdisa_12',
+                            sslmode='require')
     return conn
 
 @app.route("/")
