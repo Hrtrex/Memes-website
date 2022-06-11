d1107ea8789a9f785a80ad77cf7ab1be9cba3cf8
Bl0tniaQus <34137984+Bl0tniaQus@users.noreply.github.com>
2022-06-11 13:04:59 +0200

    obsługa zgłoszeń memów

--- App/views.py
+++ App/views.py
@@ -33,9 +33,9 @@ def user(name):
 def getZgloszenia():
     conn = get_db_connection()
     cur = conn.cursor()
-    cur.execute('select id_zgloszenia,zgloszenia_komentarzy.tresc as powod,komentarze.tresc as komentarz,czy_rozpatrzony,zgloszenia_komentarzy.komentarze_id_komentarza,zgloszenia_komentarzy.uzytkownicy_id_uzytkownika as zglaszajacyid,zgl.login as zglaszajacy,data_dodania,komentarze.uzytkownicy_id_uzytkownika as komentujacyid,kom.login as komentujacy from zgloszenia_komentarzy, komentarze,(select id_uzytkownika, login from uzytkownicy) zgl,(select id_uzytkownika,login from uzytkownicy) kom where zgloszenia_komentarzy.uzytkownicy_id_uzytkownika = zgl.id_uzytkownika and  komentarze.uzytkownicy_id_uzytkownika = kom.id_uzytkownika and zgloszenia_komentarzy.komentarze_id_komentarza = komentarze.id_komentarza order by data_dodania desc;')
+    cur.execute('select id_zgloszenia,zgloszenia_komentarzy.tresc as powod,komentarze.tresc as komentarz,czy_rozpatrzony,zgloszenia_komentarzy.komentarze_id_komentarza,zgloszenia_komentarzy.uzytkownicy_id_uzytkownika as zglaszajacyid,zgl.login as zglaszajacy,data_dodania,komentarze.uzytkownicy_id_uzytkownika as komentujacyid,kom.login as komentujacy from zgloszenia_komentarzy, komentarze,(select id_uzytkownika, login from uzytkownicy) zgl,(select id_uzytkownika,login from uzytkownicy) kom where czy_rozpatrzony=false and zgloszenia_komentarzy.uzytkownicy_id_uzytkownika = zgl.id_uzytkownika and  komentarze.uzytkownicy_id_uzytkownika = kom.id_uzytkownika and zgloszenia_komentarzy.komentarze_id_komentarza = komentarze.id_komentarza order by data_dodania desc;')
     zgloszeniakom = cur.fetchall()
-    cur.execute('select id_zgloszenia,zgloszenia_memow.tresc as powod,memy.nazwa_pliku as obraz,czy_rozpatrzony,zgloszenia_memow.memy_id_mema,zgloszenia_memow.uzytkownicy_id_uzytkownika as zglaszajacyid,zgl.login as zglaszajacy,data_dodania,memy.uzytkownicy_id_uzytkownika as wstawiajacyid,wst.login as wstawiajacy from zgloszenia_memow,memy,(select id_uzytkownika, login from uzytkownicy) zgl,(select id_uzytkownika,login from uzytkownicy) wst where zgloszenia_memow.memy_id_mema = memy.id_mema and zgloszenia_memow.uzytkownicy_id_uzytkownika = zgl.id_uzytkownika and  memy.uzytkownicy_id_uzytkownika = wst.id_uzytkownika order by data_dodania desc;')
+    cur.execute('select id_zgloszenia,zgloszenia_memow.tresc as powod,memy.nazwa_pliku as obraz,czy_rozpatrzony,zgloszenia_memow.memy_id_mema,zgloszenia_memow.uzytkownicy_id_uzytkownika as zglaszajacyid,zgl.login as zglaszajacy,data_dodania,memy.uzytkownicy_id_uzytkownika as wstawiajacyid,wst.login as wstawiajacy from zgloszenia_memow,memy,(select id_uzytkownika, login from uzytkownicy) zgl,(select id_uzytkownika,login from uzytkownicy) wst where czy_rozpatrzony=false and zgloszenia_memow.memy_id_mema = memy.id_mema and zgloszenia_memow.uzytkownicy_id_uzytkownika = zgl.id_uzytkownika and  memy.uzytkownicy_id_uzytkownika = wst.id_uzytkownika order by data_dodania desc;')
     zgloszeniamem = cur.fetchall()
     id_zgl_kom = [x[0] for x in zgloszeniakom]
     id_zgl_mem = [x[0] for x in zgloszeniamem]
@@ -45,17 +45,55 @@ def getZgloszenia():
     conn.close()
     return render_template("admin.html", zgloszeniamem = zgloszeniamem, zgloszeniakom = zgloszeniakom, id_zgl_kom = id_zgl_kom, id_zgl_mem = id_zgl_mem, n_zgl_kom = n_zgl_kom, n_zgl_mem = n_zgl_mem)
 
-@app.route("/adminAction", methods=['POST'])
-def adminAction():
-    memZamknij = request.form["mem_zamknij"]
-
+@app.route("/adminActionMem", methods=['POST'])
+def adminActionMem():
+    action_request = request.form["action"]
+    for i in range(len(action_request)):
+        if action_request[i].isdigit() == True:
+            action = action_request[:i]
+            id = action_request[i:]
+            break
     conn = get_db_connection()
     cur = conn.cursor()
-    cur.execute("delete from zgloszenia_memow where id_zgloszenia="+str(memZamknij)+";")
-    conn.commit()
+    print(action)
+    print(id)
+    if action=="zamknij":
+        cur.execute("update zgloszenia_memow set czy_rozpatrzony = true where id_zgloszenia = "+id+";")
+        conn.commit()
+    if action=="usun":
+        cur.execute("update zgloszenia_memow set czy_rozpatrzony = true where memy_id_mema in (select memy_id_mema from zgloszenia_memow where id_zgloszenia ="+id+";);")
+        conn.commit()
+        cur.execute("delete from memy where id_mema = (select memy_id_mema from zgloszenia_memow where id_zgloszenia ="+id+";);")
+        conn.commit()
+    if action=="ban":
+        ban_duration = request.form["banDuration"];
+        ban_reason ="'"+request.form["banReason"]+"'"
+        ban_enddate = "CURRENT_DATE"+" + "+ban_duration
+        cur.execute("update zgloszenia_memow set czy_rozpatrzony = true where id_zgloszenia = "+id+";")
+        conn.commit()
+        cur.execute("insert into blokady values (default,CURRENT_DATE,"+ban_enddate+","+ban_reason+","+id+");")
+        conn.commit()
     cur.close()
     conn.close()
     return redirect("/admin")
+
+@app.route("/adminActionKom", methods=['POST'])
+def adminActionKom():
+    action_request = request.form["action"]
+    for i in range(len(action_request)):
+        if action_request[i].isdigit() == True:
+            action = action_request[:i]
+            id = action_request[i:]
+            break
+    conn = get_db_connection()
+    cur = conn.cursor()
+    if action=="zamknij":
+        cur.execute("update zgloszenia_komentarzy set czy_rozpatrzony = true where id_zgloszenia = "+id+";")
+        conn.commit()      
+    cur.close()
+    conn.close()
+    return redirect("/admin")
+
 @app.route("/jbzd/", defaults={'page': ''})
 @app.route("/jbzd/<page>")
 def jbzd(page):
