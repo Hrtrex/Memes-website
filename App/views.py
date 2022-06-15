fc9762eb097677be1889a6abdd738ecd23ca4ab8
CharlesLeglerg <102187862+CharlesLeglerg@users.noreply.github.com>
2022-06-15 22:44:09 +0200

    Add files via upload

--- App/views.py
+++ App/views.py
@@ -6,7 +6,10 @@ import re
 import werkzeug
 from werkzeug.security import generate_password_hash, check_password_hash
 from App.memes import get_urls_jbzd, get_urls_kwejk
-
+from datetime import date, datetime
+import random
+import os
+from argparse import Namespace
 
 
 
@@ -55,12 +58,13 @@ def kwejk(page):
     data = list(zip(urls, votes))
     return render_template("memy.html", links=data)
 
-#Logowanie
+
+#LOGOWANIE
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     error = None
     if request.method == 'POST':
-        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
+        if request.form['username'] != 'admin' or request.form['password'] != '12345':
             error = 'Invalid Credentials. Please try again.'
         else:
             return redirect(url_for('home'))
@@ -68,11 +72,60 @@ def login():
 
 
 
-#Rejestracja
-@app.route('/register')
-def register():
 
+#REJESTRACJA
+@app.route('/register', methods = ['POST', 'GET'])
+def register():
     return render_template('register.html')
+
+
+@app.route('/userPanel', methods = ['POST', 'GET'])
+def userP():
+   
+    password = 0
+    password2 = 0
+    email = 0
+    if request.method == 'GET':
+        return f"FATAL ERROR"
+    if request.method == 'POST':
+        register_data = request.form
+        n = Namespace(**register_data)
+        user_role_test = 0
+        if n.login == 'admin':
+            user_role_test = 2
+        else:
+            user_role_test = 1
+
+    
+        if n.password != n.password2:
+            return 'Wrong password confirmation', 400
+
+        
+
+        join = date.today()
+        hashed_password = generate_password_hash(n.password)
+
+        login = n.login
+        password = hashed_password
+        email = n.email
+        user_role = user_role_test
+        joined = join
+        
+        conn = get_db_connection()
+        cur = conn.cursor()
+        cur.execute('select * from uzytkownicy')
+        cur.execute("""
+        INSERT INTO uzytkownicy (login, haslo, email, typ_uzytkownika, data_dolaczenia)
+        VALUES (%s, %s, %s, %s, %s);
+        """,
+        (login, password, email, user_role, joined))
+        
+        conn.commit()
+        cur.close()
+        conn.close()
+
+        return render_template('userPanel.html',register_data = register_data, n=n, user_role = user_role, joined=joined)
+
 
 
 #reset hasla
@@ -80,10 +133,7 @@ def register():
 def resetP():
     return render_template('passwordReset.html')
 
-#user panel
-@app.route('/userPanel')
-def userP():
-    return render_template('userPanel.html')
+
 
 
 #email ver.
