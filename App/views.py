1da28a3ab99d89958b47c66fb6d9f5b7eabfabd7
Hrtrex <59179374+Hrtrex@users.noreply.github.com>
2022-06-18 12:07:08 +0200

    user can add memes (test)

--- App/views.py
+++ App/views.py
@@ -1,7 +1,13 @@
-from flask import Flask, redirect, url_for, render_template
+import os
+from flask import Flask, redirect, url_for, render_template, request
+from werkzeug.utils import secure_filename
 from App import app
 import psycopg2
 from App.memes import Meme
+
+UPLOAD_FOLDER = 'App/static/uploads'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+ALLOWED_EXTENSIONS = {'jpg', 'jpeg', 'png'}
 
 def get_db_connection():
     conn = psycopg2.connect(host='flask-server.postgres.database.azure.com',
@@ -10,6 +16,9 @@ def get_db_connection():
                             password='Jebacdisa_12',
                             sslmode='require')
     return conn
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 @app.route("/")
 def home():
@@ -46,3 +55,23 @@ def kwejk(page):
     meme_data = Meme()
     meme_data.get_memes_kwejk(f'{page}')
     return render_template("memy.html", memes = meme_data)
+
+@app.route("/upload", methods=['GET', 'POST'])
+def upload_meme():
+    if request.method == 'POST':
+        if 'file' not in request.files:
+            return 'No meme uploaded', 400
+        file = request.files['file']
+        if file.filename == '':
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            conn = get_db_connection()
+            cur = conn.cursor()
+            cur.execute("insert into memy values (default, %s, '2022-06-18', 1, 'tytul', 'opis', 'kategoria')", (filename,))
+            conn.commit()
+            cur.close()
+            conn.close()
+            return 'Meme has been uploaded', 200
+    return render_template("test.html")
